f2cddc4112e934124cfb0a0bcec52191f26df6dd
aglis silva <aglissilva17@gmail.com>
2023-10-31 22:08:46 -0300

    removendo warning no printf do endereço de memoria

--- conteudo/ponteiros/exercicio01.c
+++ conteudo/ponteiros/exercicio01.c
@@ -9,9 +9,9 @@ int main(void)
   
   printf("valor da variavel x: %d\n", x);
   printf("valor da variavel y: %d\n", y);
-  printf("end. de memoria da variavel x: %d\n", &x);
+  printf("end. de memoria da variavel x: %p\n", &x);
   printf("ponteiro apontando o valor que esta amazenado no endereco da variavel x: %d\n", *ponteiro);
-  printf("ponteiro mostrando o endereco da variavel x: %d\n", ponteiro);
+  printf("ponteiro mostrando o endereco da variavel x: %p\n", ponteiro);
 
   *ponteiro = y;
 
